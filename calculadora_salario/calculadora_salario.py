1c3cb1a72a4eaae9fafcd547d8eaaed6cb966940
Victor Terra <victorterradev@gmail.com>
2026-02-03 18:10:36 -0300

    feat:implementa lista de funcionarios e seus salários

--- calculadora_salario/calculadora_salario.py
+++ calculadora_salario/calculadora_salario.py
@@ -1,7 +1,10 @@
+from employees import employees
+
 
 isentionLimit = 2000.0
 lowTax = 0.10
 highTax = 0.20
+
 
 
 def calc_salary(value: float) -> float:
@@ -10,15 +13,31 @@ def calc_salary(value: float) -> float:
     else:
         return value-(value * highTax)
 
-
+def add_employee():
+    new_employee = {
+        "name": entry_name,
+        "salary": liquid_salary,
+    }
+    return new_employee
 
 
 if __name__ == "__main__":
 
-    entry_salary = input("DIGITE SEU SALÁRIO:")
-    try:
-        gross_salary = float(entry_salary)
-        liquid_salary = calc_salary(gross_salary)
-        print(f"o seu salário líquido será de: R${liquid_salary:.2f} ")
-    except ValueError:
-        print("Por favor, digite um número válido ex.: 2300.50")
+    while True:
+        entry_name = input("Digite o nome do funcionário (ou 'sair' para fechar):")
+        if entry_name.lower() == "sair": break
+
+        entry_salary = input("Digite o salário do funcionário (ou 'sair' para fechar):")
+
+        try:
+            gross_salary = float(entry_salary)
+            liquid_salary = calc_salary(gross_salary)
+            employees.append(add_employee())
+
+            print(f"{entry_name}, o seu salário líquido será de: R${liquid_salary:.2f} ")
+
+        except ValueError:
+            print("Por favor, digite um número válido ex.: 2300.50")
+
+    for employee in employees:
+        print(f"Nome: {employee['name']}\nSalário: R$ {employee['salary']:.2f}")
