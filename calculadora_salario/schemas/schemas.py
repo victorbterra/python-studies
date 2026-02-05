f8de3a5d6073f055d986e6803468846d15a2e856
Victor Terra <victorterradev@gmail.com>
2026-02-05 11:11:11 -0300

    feat: implementa validação ao campo idade para que funcionários menos de 18 anos nao sejam cadastrados.

--- calculadora_salario/schemas/schemas.py
+++ calculadora_salario/schemas/schemas.py
@@ -1,9 +1,13 @@
 from pydantic import BaseModel, PositiveFloat, PositiveInt, Field
 
-class employee_schema(BaseModel):
+class EmployeeSchema(BaseModel):
     name: str = Field(
         min_length=3,
         description='O nome deve ter pelo menos 3 caracteres',
     )
-    age: int = PositiveInt
+    age: int = Field(
+        ge=18,
+        le=100,
+        description='O funcionário precisa ser maior de idade.',
+    )
     salary: float = PositiveFloat
